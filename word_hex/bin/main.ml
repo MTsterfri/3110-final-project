764f100f47480b34811d6a18c34fbb8620ca3172
Sterling Chargois <smc453@cornell.edu>
2023-10-17 17:25:44 -0400

    Implement main
    - Began implementation on main

--- word_hex/bin/main.ml
+++ word_hex/bin/main.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Hello, World!"
+open Word_hex
+open Board
+open Dictionary
+open Game
+
+let () =
+  print_endline "\n\nWelcome to Word Hex!\n";
+  print_endline
+    "How to play: Type any word you can construct from what is given.\n";
+  print_endline "Press enter to continue";
+  let _ = read_line () in
+  print_endline "Please wait while the game is set up...\n"
+(* let dict_lst = [] in let dict = HashDict.build dict_lst in let game_module =
+   Game (HexBoard) (HashDict) in *)
