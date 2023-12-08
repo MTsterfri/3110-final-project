37c080038291835ed5be7b5099cf5435375fb4f7
April Collamore <ahc248@cornell.edu>
2023-12-08 02:03:58 -0500

    Make Doc Final Touches
    - Edited the header of util.mli

--- word_hex/lib/util.mli
+++ word_hex/lib/util.mli
@@ -1,4 +1,5 @@
 (** Collection of extraneous helper functions. *)
+
 val expand : string -> char list
 (** helper function to convert a string into a list of its characters in order
     and in lowercase. Ex: explode "apples" -> ["a"; "p"; "p"; "l"; "e"; "s"]*)
